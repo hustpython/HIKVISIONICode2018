2bce4bd99f972228100b6cc5b0c8f7e2fa001ebb
Lifubang <lifubang@ciic.com.cn>
2018-05-14 15:36:37 +0800

    a naive robot for python

--- main.py
+++ main.py
@@ -1,25 +1,139 @@
 # -*- coding:utf-8 -*-
 import sys
-from UAVGoodsAIService import *
+import socket
+import json
+
+#从服务器接收一段字符串, 转化成字典的形式
+def RecvJuderData(hSocket):
+    nRet = -1
+    Message = hSocket.recv(1024 * 1024 * 4)
+    print(Message)
+    len_json = int(Message[:8])
+    str_json = Message[8:].decode()
+    if len(str_json) == len_json:
+        nRet = 0
+    Dict = json.loads(str_json)
+
+    return nRet, Dict
+
+# 接收一个字典,将其转换成json文件,并计算大小,发送至服务器
+def SendJuderData(hSocket, dict_send):
+    str_json = json.dumps(dict_send)
+    len_json = str(len(str_json)).zfill(8)
+    str_all = len_json + str_json
+    print(str_all)
+    ret = hSocket.sendall(str_all)
+    if ret == None:
+        ret = 0
+    print('sendall', ret)
+    return ret
+
+#用户自定义函数, 返回字典FlyPlane, 需要包括 "UAV_info", "purchase_UAV" 两个key.
+def AlgorithmCalculationFun(a, b, c):
+    FlyPlane = c["astUav"]
+    return FlyPlane
+
+
+
+
+
+def main(szIp, nPort, szToken):
+    print("server ip %s, prot %d, token %s\n", szIp, nPort, szToken)
+
+    #Need Test // 开始连接服务器
+    hSocket = socket.socket()
+
+    hSocket.connect((szIp, nPort))
+
+    #接受数据  连接成功后，Judger会返回一条消息：
+    nRet, _ = RecvJuderData(hSocket)
+    if (nRet != 0):
+        return nRet
+    
+
+    # // 生成表明身份的json
+    token = {}
+    token['token'] = szToken        
+    token['action'] = "sendtoken"   
+
+    
+    #// 选手向裁判服务器表明身份(Player -> Judger)
+    nRet = SendJuderData(hSocket, token)
+    if nRet != 0:
+        return nRet
+
+    #//身份验证结果(Judger -> Player), 返回字典Message
+    nRet, Message = RecvJuderData(hSocket)
+    if nRet != 0:
+        return nRet
+    
+    if Message["result"] != 0:
+        print("token check error\n")
+        return -1
+
+    # // 选手向裁判服务器表明自己已准备就绪(Player -> Judger)
+    stReady = {}
+    stReady['token'] = szToken
+    stReady['action'] = "ready"
+
+    nRet = SendJuderData(hSocket, stReady)
+    if nRet != 0:
+        return nRet
+
+    # //对战开始通知(Judger -> Player)
+    nRet, Message = RecvJuderData(hSocket)
+    if nRet != 0:
+        return nRet
+    
+    #初始化地图信息
+    pstMapInfo = Message["map"]  
+    
+    #初始化比赛状态信息
+    pstMatchStatus = {}
+    pstMatchStatus["time"] = 1
+
+    #初始化飞机状态信息
+    pstFlayPlane = {}
+    pstFlayPlane["nUavNum"] = len(pstMapInfo["init_UAV"])
+    pstFlayPlane["astUav"] = []
+
+    #每一步的飞行计划
+    FlyPlane_send = {}
+    FlyPlane_send["token"] = szToken
+    FlyPlane_send["action"] = "flyPlane"
+
+    for i in range(pstFlayPlane["nUavNum"]):
+        pstFlayPlane["astUav"].append(pstMapInfo["init_UAV"][i])
+
+    # // 根据服务器指令，不停的接受发送数据
+    while True:
+
+        # // 进行当前时刻的数据计算, 填充飞行计划，注意：1时刻不能进行移动，即第一次进入该循环时
+        # if pstMatchStatus["time"] == 0:
+        FlyPlane = AlgorithmCalculationFun(pstMapInfo, pstMatchStatus, pstFlayPlane)
+        FlyPlane_send['UAV_info'] = FlyPlane
+
+        print(pstMatchStatus["time"])
+        # //发送飞行计划
+        nRet = SendJuderData(hSocket, FlyPlane_send)
+        if nRet != 0:
+            return nRet
+        
+        # // 接受当前比赛状态
+        nRet, pstMatchStatus = RecvJuderData(hSocket)
+        if nRet != 0:
+            return nRet
+        
+        if pstMatchStatus["match_status"] == 1:
+            print("game over, we value %d, enemy value %d\n", pstMatchStatus["we_value"], pstMatchStatus["enemy_value"])
+            hSocket.close()
+            return 0
 
 if __name__ == "__main__":
     if len(sys.argv) == 4:
         print("Server Host: " + sys.argv[1])
         print("Server Port: " + sys.argv[2])
         print("Auth Token: " + sys.argv[3])
-
-        demoMap = r'''{"map": 
-        {
-            "x": 100,  
-            "y": 100,
-            "z": 100,
-            "comment": "Hello, AI!"
-        }
-        }
-        '''
-        ai = UAVGoodsAIService(sys.argv[1], sys.argv[2], sys.argv[3])
-        print(ai.sayHello(demoMap))
-        print("testing numpy:")
-        ai.numpyDemo()
+        main(sys.argv[1], int(sys.argv[2]), sys.argv[3])
     else:
         print("need 3 arguments")
