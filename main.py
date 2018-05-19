7301c27eb3af48c4ad25f821e763f23a560b6793
mxq <1529806383@qq.com>
2018-05-19 17:54:54 +0800

    some changes

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import sys
 import socket
 import json
-# python main.py 47.95.243.246 31868 53eda47f-a90f-417d-be15-30daac19cd66
+# python main.py 47.95.243.246 31343 53eda47f-a90f-417d-be15-30daac19cd66
 #从服务器接收一段字符串, 转化成字典的形式
 def RecvJuderData(hSocket):
     nRet = -1
@@ -34,95 +34,140 @@ def SendJuderData(hSocket, dict_send):
     return ret
 
 # 用户自定义函数, 返回字典FlyPlane, 需要包括 "UAV_info", "purchase_UAV" 两个key.
+class Algo():
+    def __init__(self):
+        self.request_down = []
+        self.good_selectno = -1
+        self.good_needup = []
+        self.good_needdown = []
+        self.good_to = [0,0]
+    def AlgorithmCalculationFun(self,a, b, c):
+        FlyPlane = c["astUav"]
+        #parse mapinfo
+        # restricts
 
-def AlgorithmCalculationFun(a, b, c):
-    FlyPlane = c["astUav"]
-    #parse mapinfo
-    # restricts
-    flayhlow = a["h_low"]
-    flayhhight = a["h_high"]
+        flayhlow = a["h_low"]
+        flayhhight = a["h_high"]
 
-    buildings = [{"x_start":buildinfo["x"],"x_end":buildinfo["x"]+buildinfo["l"]-1,\
-                 "y_start":buildinfo["y"],"y_end":buildinfo["y"]+buildinfo["w"]-1,
-                 "z_start":0,"z_end":buildinfo["h"]}\
-                 for buildinfo in a["building"]]
-    fogs = [{"x_start":foginfo["x"],"x_end":foginfo["x"]+foginfo["l"]-1,\
-            "y_start":foginfo["y"],"y_end":foginfo["y"]+foginfo["w"]-1,
-            "z_start":foginfo["b"],"z_end":foginfo["t"]}\
-            for foginfo in a["fog"]]
+        buildings = [{"x_start":buildinfo["x"],"x_end":buildinfo["x"]+buildinfo["l"]-1,\
+                    "y_start":buildinfo["y"],"y_end":buildinfo["y"]+buildinfo["w"]-1,
+                    "z_start":0,"z_end":buildinfo["h"]}\
+                    for buildinfo in a["building"]]
+        fogs = [{"x_start":foginfo["x"],"x_end":foginfo["x"]+foginfo["l"]-1,\
+                "y_start":foginfo["y"],"y_end":foginfo["y"]+foginfo["w"]-1,
+                "z_start":foginfo["b"],"z_end":foginfo["t"]}\
+                for foginfo in a["fog"]]
 
-    uavloadweight = {}
-    for i in a["UAV_price"]:
-        uavloadweight[i["type"]] = i["load_weight"]
-    #print(FlyPlane)
-    # 如果uav是寻找goods的起始点，则应该传入起始位置
-    # 若uav正在去送goods到终点，则应该传入终点位置
-    # map(),pool 多线程
-    def MoveandJudge(FlyPlane):
-        # 垂直上升
-        z_status = [sin_z["z"] for sin_z in FlyPlane]
-        for i,_ in enumerate(FlyPlane):
-            z = FlyPlane[i]["z"]
-            if z < flayhlow and (z+1) not in z_status:
-                FlyPlane[i]["z"] += 1
-                z_status[i] += 1
-            else:
-                FlyPlane[i]["x"] += 1
-        # 水平方向的６种飞行方式
-        '''horizonfly_modes = [[x+1,y],[x-1,y],[x,y+1],[x,y-1],[x+1,y+1],[x-1,y-1]]
+        uavloadweight = {}
+        for i in a["UAV_price"]:
+            uavloadweight[i["type"]] = i["load_weight"]
+        #print(FlyPlane)
+        # 如果uav是寻找goods的起始点，则应该传入起始位置
+        # 若uav正在去送goods到终点，则应该传入终点位置
+        # map(),pool 多线程
         
-        feasiblefly_modes = []
-        # 去除操作后会撞墙的 mode
-        for every_mode in horizonfly_modes:
-            flag = 1
-            for every_build in buildings:
-               if z < every_build["z_end"] and every_mode[0]>= every_build["x_start"] and \
-                  every_mode[0]<= every_build["x_end"] \
-                  and every_mode[1] >= every_build["y_start"] and every_mode[1] <= every_build["y_end"]:
-                  flag = 0
-            if flag:
-                feasiblefly_modes.append(every_mode)
-        
-        # 利用欧氏距离找出最短路径 mode 
-        # 分达到 货物 起点和 货物目的地两种情况
-        dis_max = 100000
-        short_mode = []
-        for every_mode in feasiblefly_modes:
-            dis = (x_end - every_mode[0])**2 + (y_end - every_mode[1])
-            if dis < dis_max:
-                dis_max = dis 
-                short_mode = every_mode'''
-    # 购买uav
-    def toPurchaseUav():
-        pass
-        
-
-
             
-
+            # 水平方向的６种飞行方式
+            '''horizonfly_modes = [[x+1,y],[x-1,y],[x,y+1],[x,y-1],[x+1,y+1],[x-1,y-1]]
             
+            feasiblefly_modes = []
+            # 去除操作后会撞墙的 mode
+            for every_mode in horizonfly_modes:
+                flag = 1
+                for every_build in buildings:
+                if z < every_build["z_end"] and every_mode[0]>= every_build["x_start"] and \
+                    every_mode[0]<= every_build["x_end"] \
+                    and every_mode[1] >= every_build["y_start"] and every_mode[1] <= every_build["y_end"]:
+                    flag = 0
+                if flag:
+                    feasiblefly_modes.append(every_mode)
+            
+            # 利用欧氏距离找出最短路径 mode 
+            # 分达到 货物 起点和 货物目的地两种情况
+            dis_max = 100000
+            short_mode = []
+            for every_mode in feasiblefly_modes:
+                dis = (x_end - every_mode[0])**2 + (y_end - every_mode[1])
+                if dis < dis_max:
+                    dis_max = dis 
+                    short_mode = every_mode'''
+        # 购买uav
+        def toPurchaseUav(self):
+            pass
+            
+        #parse matchstatus
+        # good_no 出现 -1 的情况：
+        # 1.在停机坪或故意不运送货物
+        # 2.运送途中出现撞机，且status = 1.
+        # 3.将货物运送到目的地后，在寻找下一个货物地点中
         
+        if b["time"] >= 1:
+        # value of us:b["we_value"]
+            UAV_we = b["UAV_we"]
+            carryandnormal = [i for i in UAV_we if i["goods_no"]!= -1 and i["status"] == 0]
+            notcarryandnormal = [i for i in UAV_we if i["goods_no"] == -1 and i["status"] == 0]
         
-        
-        
-    #parse matchstatus
-    # good_no 出现 -1 的情况：
-    # 1.在停机坪或故意不运送货物
-    # 2.运送途中出现撞机，且status = 1.
-    # 3.将货物运送到目的地后，在寻找下一个货物地点中
-    if b["time"] != 0:
-       # value of us:b["we_value"]
-       UAV_we = b["UAV_we"]
-       carryandnormal = [i for i in UAV_we if i["goods_no"]!= -1 and i["status"] == 0]
-       notcarryandnormal = [i for i in UAV_we if i["goods_no"] == -1 and i["status"] == 0]
-       
-       goods = b["goods"]
-       uav_info = []
-       for uav in notcarryandnormal:
-           for good in goods:
-              if uavloadweight[uav["type"]] > good["weight"]:
-                  MoveandJudge(FlyPlane)
-    return FlyPlane
+            goods = b["goods"]
+            for uav in notcarryandnormal:
+                for good in goods:
+                    if uavloadweight[uav["type"]] > good["weight"]:
+                        pass
+            # 垂直上升,一架一架的离开，直达所有飞机到达最低高度
+            z_status = [sin_z["z"] for sin_z in FlyPlane]
+            for i,_ in enumerate(FlyPlane):
+                z = FlyPlane[i]["z"]
+                no = FlyPlane[i]["no"]
+                if no in self.good_needup and z<= flayhlow:
+                    FlyPlane[i]["z"] += 1
+
+                elif no in self.good_needup and z > flayhlow:
+                    x_dis = self.good_to[0] - FlyPlane[i]["x"]
+                    y_dis = self.good_to[1] - FlyPlane[i]["y"]
+                    if x_dis != 0:
+                        FlyPlane[i]["x"] += int(x_dis/(abs(x_dis)))
+                    if y_dis != 0:
+                        FlyPlane[i]["y"] += int(y_dis/(abs(y_dis)))
+                    if x_dis == 0 and y_dis == 0:
+                        self.good_needup.remove(no)
+                        if no not in self.good_needdown:
+                           self.good_needdown.append(no)
+                elif z <= flayhlow and (z+1) not in z_status and no not in self.request_down:
+                    if i == 0:
+                        FlyPlane[i]["z"] += 1
+                        z_status[i] += 1
+                elif z> flayhlow and no not in self.good_needdown:
+                    if i== 0:
+                        z_status[i] = -1
+                        dis = [(good["start_x"] - FlyPlane[i]["x"])**2 + (good["start_y"] - FlyPlane[i]["y"])**2 for good in goods]
+                        min_dis_index = dis.index(min(dis))
+                        x_dis = goods[min_dis_index]["start_x"] - FlyPlane[i]["x"]
+                        y_dis = goods[min_dis_index]["start_y"] - FlyPlane[i]["y"]
+                        if x_dis != 0:
+                           FlyPlane[i]["x"] += int(x_dis/(abs(x_dis)))
+                        if y_dis != 0:
+                           FlyPlane[i]["y"] += int(y_dis/(abs(y_dis)))
+                        if x_dis == 0 and x_dis == 0:
+                            if FlyPlane[i]["no"] not in self.request_down:
+                                self.request_down.append(FlyPlane[i]["no"])
+                            FlyPlane[i]["z"] -= 1
+                            self.good_selectno = goods[min_dis_index]["no"]
+                            self.good_to = [goods[min_dis_index]["end_x"],goods[min_dis_index]["end_y"]]
+                elif no in self.good_needdown:
+                    if z != 0  and i == 0:
+                        FlyPlane[i]["z"] -= 1
+                    if z == 0:
+                        FlyPlane[i]["goods_no"] = -1
+                        #FlyPlane[i]["z"] += 1
+                elif no in self.request_down and no not in self.good_needdown:
+                    if z != 0  and i == 0:
+                        FlyPlane[i]["z"] -= 1
+                    if z == 0:
+                        FlyPlane[i]["goods_no"] = self.good_selectno
+                        if FlyPlane[i]["no"] not in self.good_needup:
+                           self.good_needup.append(FlyPlane[i]["no"])
+                            
+        print(FlyPlane[0])              
+        return FlyPlane
 
 
 
@@ -207,6 +252,7 @@ def main(szIp, nPort, szToken):
         pstFlayPlane["astUav"].append(pstMapInfo["init_UAV"][i])
 
     # // 根据服务器指令，不停的接受发送数据
+    Algo_main = Algo()
     while True:
 
         # // 进行当前时刻的数据计算, 填充飞行计划，注意：1时刻不能进行移动，
@@ -214,7 +260,8 @@ def main(szIp, nPort, szToken):
         # pstMapInfo 在一场比赛中是固定的，不会变
         # pstMatchStatus 服务器根据用户发送的数据经过处理后返回的信息
         # pstFlayPlane 是用户根据服务器的数据，经过自己的算法计算后得到的作战计划，需要发送给服务器
-        FlyPlane = AlgorithmCalculationFun(pstMapInfo, pstMatchStatus, pstFlayPlane)
+        
+        FlyPlane = Algo_main.AlgorithmCalculationFun(pstMapInfo, pstMatchStatus, pstFlayPlane)
         FlyPlane_send['UAV_info'] = FlyPlane
         #print(pstMatchStatus["time"])
         #==================mxq=====================
